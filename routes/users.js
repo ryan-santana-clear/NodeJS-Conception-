5f1d84af293ccbe534ab43dc132f471933f6ad23
Ryan Santana <ryan.santana.clear@gmail.com>
2023-02-28 12:43:40 -0300

    Feat: Implementing editing of user data

--- routes/users.js
+++ routes/users.js
@@ -60,4 +60,18 @@ module.exports = (app)=>{
 
     });
 
+    routeId.put((req, res) =>{
+
+        db.update({_id:req.params.id}, req.body, err =>{
+
+            if (err) {
+                app.utils.error.send(err,req, res);
+            } else {
+                res.status(200).json(Object.assign(req.params, req.body));
+            }
+
+        });
+
+    });
+
 };
