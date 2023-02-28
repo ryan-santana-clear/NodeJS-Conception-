d6de164e5084351c7e211ef1f5683a1294a9c407
Ryan Santana <ryan.santana.clear@gmail.com>
2023-02-28 10:03:19 -0300

    Feat: Separating Routes from the Main File

---
+++ routes/users.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let routes = express.Router();
+
+
+routes.get('/',(req, res) => {
+    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users:[{
+            name:'Test',
+            email:'test@test.com.br',
+            id:1
+        }]
+    });
+
+});
+
+routes.get('/admin',(req, res) => {
+    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users:[]
+    });
+
+});
+
+module.exports = routes;
